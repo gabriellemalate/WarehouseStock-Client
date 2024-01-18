3e9570d35607d2590d9fa32f431dc297341c9ace
Gabrielle Malate <gabriellemalate@gmail.com>
2024-01-17 19:18:00 -0800

    buil t warehouse option component

---
+++ src/components/WarehouseOption/WarehouseOption.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Trash from "../../assets/icons/delete_outline-24px.svg";
+import Pencil from "../../assets/icons/edit-24px.svg";
+import Right from "../../assets/icons/chevron_right-24px.svg"
+import "./WarehouseOption.scss"
+
+function WarehouseOption() {
+
+    return (
+        <>
+            <article className="warehouses-list__item">
+                <div className="warehouses-list__item-eq">
+                    <div className="warehouses-list__item-container">
+                        <div className="warehouses-list__item-core">
+                            <div className="warehouses-list__items warehouses-list__items-left">
+                                <div className="warehouses-list__item-detail">
+                                    <h4 className="warehouses-list__item-detail-label">WAREHOUSE</h4>
+                                    <p className="warehouses-list__item-detail-value warehouses-list__item-detail-warehouse"> Value
+                                        <img className="warehouses-list__item-detail-arrow" src={Right} alt="See More" />
+                                    </p>
+                                </div>
+                                <div className="warehouses-list__item-detail">
+                                    <h4 className="warehouses-list__item-detail-label">ADDRESS</h4>
+                                    <p className="warehouses-list__item-detail-value warehouses-list__item-detail-address">503 Broadway, New York, USA
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="warehouses-list__items warehouses-list__items-right">
+                                <div className="warehouses-list__item-detail">
+                                    <h4 className="warehouses-list__item-detail-label">CONTACT NAME</h4>
+                                    <p className="warehouses-list__item-detail-value warehouses-list__item-detail-contact">Value Value
+                                    </p>
+                                </div>
+                                <div className="warehouses-list__item-detail">
+                                    <h4 className="warehouses-list__item-detail-label">CONTACT INFORMATION</h4>
+                                    <p className="warehouses-list__item-detail-value warehouses-list__item-detail-value-info">
+                                        <span className="warehouses-list__item-detail-phone"> +1 (629) 555-0129</span>
+                                        <span className="warehouses-list__item-detail-email">paujla@instock.com </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="warehouses-list__actions">
+                            <img src={Trash} alt="Delete" />
+                            <img src={Pencil} alt="Edit" />
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </>
+    );
+}
+
+export default WarehouseOption;
--- src/pages/WarehouseListPage/WarehouseListPage.js
+++ src/pages/WarehouseListPage/WarehouseListPage.js
@@ -2,11 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import MagnifyingGlass from "../../assets/icons/search-24px.svg";
-import Trash from "../../assets/icons/delete_outline-24px.svg";
-import Pencil from "../../assets/icons/edit-24px.svg";
 import Sort from "../../assets/icons/sort-24px.svg";
-import Right from "../../assets/icons/chevron_right-24px.svg"
 import "./WarehouseListPage.scss"
+import WarehouseOption from "../../components/WarehouseOption/WarehouseOption";
 
 function WarehouseListPage() {
 
@@ -56,46 +54,7 @@ function WarehouseListPage() {
                                     ACTIONS
                                 </h3>
                             </article>
-
-                            <article className="warehouses-list__item">
-                                <div className="warehouses-list__item-eq">
-                                    <div className="warehouses-list__item-container">
-                                        <div className="warehouses-list__item-core">
-                                            <div className="warehouses-list__items warehouses-list__items-left">
-                                                <div className="warehouses-list__item-detail">
-                                                    <h4 className="warehouses-list__item-detail-label">WAREHOUSE</h4>
-                                                    <p className="warehouses-list__item-detail-value warehouses-list__item-detail-warehouse"> Value
-                                                        <img className="warehouses-list__item-detail-arrow" src={Right} alt="See More" />
-                                                    </p>
-                                                </div>
-                                                <div className="warehouses-list__item-detail">
-                                                    <h4 className="warehouses-list__item-detail-label">ADDRESS</h4>
-                                                    <p className="warehouses-list__item-detail-value warehouses-list__item-detail-address">503 Broadway, New York, USA
-                                                    </p>
-                                                </div>
-                                            </div>
-                                            <div className="warehouses-list__items warehouses-list__items-right">
-                                                <div className="warehouses-list__item-detail">
-                                                    <h4 className="warehouses-list__item-detail-label">CONTACT NAME</h4>
-                                                    <p className="warehouses-list__item-detail-value warehouses-list__item-detail-contact">Value Value
-                                                    </p>
-                                                </div>
-                                                <div className="warehouses-list__item-detail">
-                                                    <h4 className="warehouses-list__item-detail-label">CONTACT INFORMATION</h4>
-                                                    <p className="warehouses-list__item-detail-value warehouses-list__item-detail-value-info">
-                                                        <span className="warehouses-list__item-detail-phone"> +1 (629) 555-0129</span>
-                                                        <span className="warehouses-list__item-detail-email">paujla@instock.com </span>
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="warehouses-list__actions">
-                                            <img src={Trash} alt="Delete" />
-                                            <img src={Pencil} alt="Edit" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </article>
+                            <WarehouseOption/>
                         </div>
                     </section>
                 </div>
