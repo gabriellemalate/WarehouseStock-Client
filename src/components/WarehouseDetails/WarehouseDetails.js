ee8addfc28188ba635bd25f2634aa74c6699f0fc
Kyriakos Bakalis <greekgodsocrates.com>
2024-01-18 11:43:52 -0500

    first commit

---
+++ src/components/WarehouseDetails/WarehouseDetails.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Trash from "../../assets/icons/delete_outline-24px.svg";
+import Pencil from "../../assets/icons/edit-24px.svg";
+import Right from "../../assets/icons/chevron_right-24px.svg"
+import "./WarehouseDetails.scss"
+
