7f62f115fe466dace8b27792649fce4bd8236610
Soham Gaggenapally <55066921+sgaggen@users.noreply.github.com>
2024-01-19 13:16:10 -0500

    Add conditional formatting for item status

--- src/pages/InventoryPage/InventoryPage.js
+++ src/pages/InventoryPage/InventoryPage.js
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState, useRef } from "react"
 import MagnifyingGlass from "../../assets/icons/search-24px.svg";
@@ -34,26 +34,41 @@ function InventoryPage() {
         <main className="inventory">
 
             <section className="inventory-container inventory__title-bar">
-                    <h1>Inventory</h1>
-                    <div className="inventory__actions">
-                        <form className="inventory__search">
-                            <img className="inventory__search-icon" src={MagnifyingGlass} alt="Search" />
-                            <input className="inventory__search-field" type="search" placeholder="Search..." />
-                        </form>
-                        <button>+ Add New Item</button>
+                <h1>Inventory</h1>
+                <div className="inventory__actions">
+                    <form className="inventory__search">
+                        <img className="inventory__search-icon" src={MagnifyingGlass} alt="Search" />
+                        <input className="inventory__search-field" type="search" placeholder="Search..." />
+                    </form>
+                    <button>+ Add New Item</button>
                 </div>
             </section>
 
             <article className="inventory__list">
                 <div className="inventory-container inventory__list-header">
                     <div className="inventory__item-info--left">
-                        <h4 className="inventory__item-label">INVENTORY ITEM</h4>
-                        <h4 className="inventory__item-label">CATEGORY</h4>
+                        <h4 className="inventory__item-label">
+                            INVENTORY ITEM
+                            <img className="inventory__item-label--sort" src={Sort} alt="Sort Column" />
+                        </h4>
+                        <h4 className="inventory__item-label">
+                            CATEGORY
+                            <img className="inventory__item-label--sort" src={Sort} alt="Sort Column" />
+                        </h4>
                     </div>
                     <div className="inventory__item-info--center">
-                        <h4 className="inventory__item-label">STATUS</h4>
-                        <h4 className="inventory__item-label">QTY</h4>
-                        <h4 className="inventory__item-label">WAREHOUSE</h4>
+                        <h4 className="inventory__item-label">
+                            STATUS
+                            <img className="inventory__item-label--sort" src={Sort} alt="Sort Column" />
+                        </h4>
+                        <h4 className="inventory__item-label">
+                            QTY
+                            <img className="inventory__item-label--sort" src={Sort} alt="Sort Column" />
+                        </h4>
+                        <h4 className="inventory__item-label">
+                            WAREHOUSE
+                            <img className="inventory__item-label--sort" src={Sort} alt="Sort Column" />
+                        </h4>
                     </div>
                     <div className="inventory__item-info--right">
                         <h4 className="inventory__item-label">ACTIONS</h4>
@@ -62,34 +77,34 @@ function InventoryPage() {
                 {inventoryItems.map(item => (
                     <article className='inventory-container inventory__list-item' key={item.id}>
                         {/* <div className=""> */}
-                            <div className="inventory__item-info">
-                                <div className="inventory__item-info--left">
-                                    <h4 className="inventory__item-label inventory__item-label--mobile">INVENTORY ITEM</h4>
-                                    <Link to={`/inventory/${item.id}`} className='inventory__item-label--title'>
-                                        <h3 className="inventory__item-label--title">{item.item_name}</h3>
-                                        <img className="inventory__item-label--arrow" src={RightArrow} alt="See More" />
-                                        {/* <span><img className="inventory__item-label--arrow" src={RightArrow} alt="See More" /></span> */}
-                                    </Link>
-                                    <h4 className="inventory__item-label inventory__item-label--mobile">CATEGORY</h4>
-                                    <p className="inventory__item-label--text">{item.category}</p>
-                                </div>
-                                <div className="inventory__item-info--center">
-                                    <h4 className="inventory__item-label inventory__item-label--mobile">STATUS</h4>
-                                    <p className="inventory__item-label--text">{item.status}</p>
-                                    <h4 className="inventory__item-label inventory__item-label--mobile">QTY</h4>
-                                    <p className="inventory__item-label--text">{item.quantity}</p>
-                                    <h4 className="inventory__item-label inventory__item-label--mobile">WAREHOUSE</h4>
-                                    <p className="inventory__item-label--text">{item.warehouse_name}</p>
-                                </div>
-                            </div>
-                            <div className="inventory__item-actions">
-                                <Link to={`/inventory/${item.id}/delete`} className=''>
-                                    <img src={Delete} alt="Delete Icon" />
+                        <div className="inventory__item-info">
+                            <div className="inventory__item-info--left">
+                                <h4 className="inventory__item-label inventory__item-label--mobile">INVENTORY ITEM</h4>
+                                <Link to={`/inventory/${item.id}`} className='inventory__item-label--title'>
+                                    <h3 className="inventory__item-label--title">{item.item_name}</h3>
+                                    <img className="inventory__item-label--arrow" src={RightArrow} alt="See More" />
+                                    {/* <span><img className="inventory__item-label--arrow" src={RightArrow} alt="See More" /></span> */}
                                 </Link>
-                                <Link to={`/inventory/${item.id}/edit`} className=''>
-                                    <img src={Edit} alt="Edit Icon" />
-                                </Link>
+                                <h4 className="inventory__item-label inventory__item-label--mobile">CATEGORY</h4>
+                                <p className="inventory__item-label--text">{item.category}</p>
                             </div>
+                            <div className="inventory__item-info--center">
+                                <h4 className="inventory__item-label inventory__item-label--mobile">STATUS</h4>
+                                <p className={"inventory__item-label--text tag--" + (item.quantity ? "in-stock" : "out-of-stock")}>{item.status.toUpperCase()}</p>
+                                <h4 className="inventory__item-label inventory__item-label--mobile">QTY</h4>
+                                <p className="inventory__item-label--text">{item.quantity}</p>
+                                <h4 className="inventory__item-label inventory__item-label--mobile">WAREHOUSE</h4>
+                                <p className="inventory__item-label--text">{item.warehouse_name}</p>
+                            </div>
+                        </div>
+                        <div className="inventory__item-actions">
+                            <Link to={`/inventory/${item.id}/delete`} className=''>
+                                <img src={Delete} alt="Delete Icon" />
+                            </Link>
+                            <Link to={`/inventory/${item.id}/edit`} className=''>
+                                <img src={Edit} alt="Edit Icon" />
+                            </Link>
+                        </div>
                         {/* </div> */}
                     </article>
 
