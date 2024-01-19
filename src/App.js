b7e6d02ebac84901f8e329e3d2092d8ca410207a
Gabrielle Malate <gabriellemalate@gmail.com>
2024-01-19 00:15:23 -0800

    initiated page files

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ import WarehouseListPage from "./pages/WarehouseListPage/WarehouseListPage"
                         <Route path="/add" element={<WarehouseAddPage />} />
                         <Route path="/inventory" element={<InventoryPage />} />
                         <Route path="/inventory/:itemId" element={<ItemPage />} />
-                        <Route path="/inventory/:itemId/edit" element={<ItemEditPage />} />
+                        <Route path="/inventory/:itemId/edit" element={<ItemEditPage />} /> */}
                         <Route path="/inventory/add" element={<ItemAddPage />} />
-                        <Route path="*" element={<NotFoundPage />} /> */}
+                        {/* <Route path="*" element={<NotFoundPage />} /> */}
                     </Routes>
                     {/* <Footer /> */}
                 </BrowserRouter>
---
+++ src/pages/InventoryAdd/InventoryAdd.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./InventoryAdd.scss"
+
+function InventoryAdd() {
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default InventoryAdd;
