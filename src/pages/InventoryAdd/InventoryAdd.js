c6efa56854698e21b167002fe56781c059de02ca
Gabrielle Malate <gabriellemalate@gmail.com>
2024-01-19 10:06:27 -0800

    started handlers

--- src/pages/InventoryAdd/InventoryAdd.js
+++ src/pages/InventoryAdd/InventoryAdd.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./InventoryAdd.scss";
 import Back from "../../assets/icons/arrow_back-24px.svg";
 import Down from "../../assets/icons/arrow_drop_down-24px.svg";
+import { Link } from "react-router-dom";
 
 function InventoryAdd() {
+    const [itemName, setItemName] = useState("");
+    const [warehouse, setWarehouse] = useState("");
+    const [inputErrors, setInputErrors] = useState({
+        item: "",
+        warehouse: "",
+    });
+
+    const handleSubmit = () => {
+
+        if (itemName.trim() === "") {
+            setInputErrors((prev) => ({ ...prev, item: "This field is required" }));
+            return;
+        } else {
+            setInputErrors((prev) => ({ ...prev, item: "This field is required" }));
+        }
+
+        if (warehouse === "") {
+            setInputErrors((prev) => ({ ...prev, warehouse: true }));
+            return;
+        }
+    };
 
     return (
         <>
@@ -15,63 +37,80 @@ function InventoryAdd() {
                             <h1 className="inv-add-top__title">Add New Inventory Item</h1>
                         </div>
                     </article>
-                    <div className="inv-add__sections">
-                        <section className="inv-add-details">
-                            <div className="inv-add-details-eq">
-                                <h2 className="inv-add__head">Item Details</h2>
-                                <div className="inv-add-details__name">
-                                    <h3 className="inv-add-label">Item Name</h3>
-                                    <textarea className="inv-add-details__name-input" placeholder="Item Name" />
-                                </div>
-                                <div className="inv-add-details__descrip">
-                                    <h3 className="inv-add-label">Description</h3>
-                                    <textarea className="inv-add-details__descrip-input" placeholder="Please enter a brief item description..." />
-                                </div>
-                                <div className="inv-add-details__category">
-                                    <h3 className="inv-add-label">Category</h3>
-                                    <select className="inv-add-details__category-input">
-                                        <option value="" disabled selected className="inv-add-details__category-input-disabled">Please select</option>
-                                    </select>
-                                    <img src={Down} className="inv-add-details__category-icon" />
-                                </div>
-                            </div>
-                        </section>
-                        <section className="inv-add-avail">
-                            <div className="inv-add-avail-eq">
-                                <h2 className="inv-add__head inv-add-avail__head">Item Availability</h2>
-                                <article className="inv-add-avail__stat">
-                                    <h3 className="inv-add-label">Status</h3>
-                                    <div className="inv-add-avail__stat-radios">
-                                        <div className="inv-add-avail__stat-in">
-                                            <input type="radio" id="instock" className="inv-add-avail__stat-in-radio"/>
-                                            <label className="inv-add-avail__stat-in-label" htmlFor="instock">In stock</label>
-                                        </div>
-                                        <div className="inv-add-avail__stat-out">
-                                            <input type="radio" id="outstock" className="inv-add-avail__stat-out-radio"/>
-                                            <label className="inv-add-avail__stat-in-label" htmlFor="outstock">Out of stock</label>
-                                        </div>
+                    <form className="inv-add__form">
+                        <div className="inv-add__sections">
+                            <section className="inv-add-details">
+                                <div className="inv-add-details-eq">
+                                    <h2 className="inv-add__head">Item Details</h2>
+                                    <div className="inv-add-details__name">
+                                        <h3 className="inv-add-label">Item Name</h3>
+                                        <textarea
+                                            className={`inv-add-details__name-input ${inputErrors.item ? "error" : ""}`}
+                                            placeholder="Item Name"
+                                            value={itemName}
+                                            onChange
+                                        />
                                     </div>
-                                </article>
-                                <div className="inv-add-avail__quantity">
-                                    <h3 className="inv-add-label">Quantity</h3>
-                                    <input className="inv-add-avail__quantity-input" type="number" placeholder="0" />
+                                    <div className="inv-add-details__descrip">
+                                        <h3 className="inv-add-label">Description</h3>
+                                        <textarea className="inv-add-details__descrip-input" placeholder="Please enter a brief item description..." />
+                                    </div>
+                                    <div className="inv-add-details__category">
+                                        <h3 className="inv-add-label">Category</h3>
+                                        <select className="inv-add-details__category-input">
+                                            <option value="" disabled selected className="inv-add-details__category-input-disabled">Please select</option>
+                                        </select>
+                                        <img src={Down} className="inv-add-details__category-icon" />
+                                    </div>
                                 </div>
-                                <div className="inv-add-avail__ware">
-                                    <h3 className="inv-add-label">Warehouse</h3>
-                                    <select className="inv-add-avail__ware-input">
-                                        <option>Please select</option>
-                                    </select>
-                                    <img src={Down} className="inv-add-avail__ware-icon" />
+                            </section>
+                            <section className="inv-add-avail">
+                                <div className="inv-add-avail-eq">
+                                    <h2 className="inv-add__head inv-add-avail__head">Item Availability</h2>
+                                    <article className="inv-add-avail__stat">
+                                        <h3 className="inv-add-label">Status</h3>
+                                        <div className="inv-add-avail__stat-radios">
+                                            <div className="inv-add-avail__stat-in">
+                                                <input type="radio" id="instock" className="inv-add-avail__stat-in-radio" />
+                                                <label className="inv-add-avail__stat-in-label" htmlFor="instock">In stock</label>
+                                            </div>
+                                            <div className="inv-add-avail__stat-out">
+                                                <input type="radio" id="outstock" className="inv-add-avail__stat-out-radio" />
+                                                <label className="inv-add-avail__stat-in-label" htmlFor="outstock">Out of stock</label>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <div className="inv-add-avail__quantity">
+                                        <h3 className="inv-add-label">Quantity</h3>
+                                        <input className="inv-add-avail__quantity-input" type="number" placeholder="0" />
+                                    </div>
+                                    <div className="inv-add-avail__ware">
+                                        <h3 className="inv-add-label">Warehouse</h3>
+                                        <select
+                                            className={`inv-add-avail__ware-input ${inputErrors.warehouse ? "error" : ""}`}
+                                            value={warehouse}
+                                            onChange
+                                        >
+                                            <option>Please select</option>
+                                        </select>
+                                        <img src={Down} className="inv-add-avail__ware-icon" />
+                                    </div>
                                 </div>
-                            </div>
-                        </section>
-                    </div>
-                    <article className="inv-add-buttons">
-                        <div className="inv-add-buttons-eq">
-                            <button className="inv-add-buttons-cancel">Cancel</button>
-                            <button className="inv-add-buttons-add">+ Add Item</button>
+                            </section>
                         </div>
-                    </article>
+                        <article className="inv-add-buttons">
+                            <div className="inv-add-buttons-eq">
+                                <Link to="/inventory" className="inv-add-buttons-cancel" >Cancel</Link>
+                                <button
+                                    type="submit"
+                                    className="inv-add-buttons-add"
+                                    onClick={handleSubmit}
+                                >
+                                    + Add Item
+                                </button>
+                            </div>
+                        </article>
+                    </form>
                 </div>
             </main>
         </>
