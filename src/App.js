3b783761c5cb79823c3e46ea3ac05a23c216af8b
Gabrielle Malate <gabriellemalate@gmail.com>
2024-01-19 00:16:29 -0800

    routed path to page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.scss';
 import Header from './components/Header/Header';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import WarehouseListPage from "./pages/WarehouseListPage/WarehouseListPage"
+import WarehouseListPage from "./pages/WarehouseListPage/WarehouseListPage";
+import InventoryAdd from "./pages/InventoryAdd/InventoryAdd"
 
     function App() {
         return (
@@ -16,7 +17,7 @@ import WarehouseListPage from "./pages/WarehouseListPage/WarehouseListPage"
                         <Route path="/inventory" element={<InventoryPage />} />
                         <Route path="/inventory/:itemId" element={<ItemPage />} />
                         <Route path="/inventory/:itemId/edit" element={<ItemEditPage />} /> */}
-                        <Route path="/inventory/add" element={<ItemAddPage />} />
+                        <Route path="/inventory/add" element={<InventoryAdd />} />
                         {/* <Route path="*" element={<NotFoundPage />} /> */}
                     </Routes>
                     {/* <Footer /> */}
