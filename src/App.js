85328e4b4808327ea942c2c2b937e3577434ebd6
Kyriakos Bakalis <greekgodsocrates.com>
2024-01-19 19:51:49 -0500

    pull from develop latest

--- src/App.js
+++ src/App.js
@@ -4,33 +4,31 @@ import Header from './components/Header/Header';
 import WarehouseListPage from "./pages/WarehouseListPage/WarehouseListPage";
 import WarehouseEditPage from './pages/WarehouseEditPage/WarehouseEditPage';
 import WarehouseAddPage from './pages/WarehouseAddPage/WarehouseAddPage';
+import WarehouseDelete from './components/WarehouseDelete/WarehouseDelete';
 import Footer from "./components/Footer/Footer";
 import './App.scss';
 
-    function App() {
-        return (
-            <>
-                <BrowserRouter>
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<WarehouseListPage />} />
-                        {/* <Route path="/:warehouseId" element={<WarehousePage />} />
-                        <Route path="/:warehouseId/edit" element={<WarehouseEditPage />} />
-                        <Route path="/add" element={<WarehouseAddPage />} />
-                        <Route path="/inventory" element={<InventoryPage />} />
-                        <Route path="/warehouse" element={<WarehouseListPage />} />
-                        <Route path="/warehouse/add" element={<WarehouseAddPage />} />
-                        {/* <Route path="/warehouse/:warehouseId" element={<WarehousePage />} /> */}
-                        <Route path="/warehouse/:warehouseId/edit" element={<WarehouseEditPage />} />
-                        {/* <Route path="/inventory/:itemId" element={<ItemPage />} />
-                        <Route path="/inventory/:itemId/edit" element={<ItemEditPage />} />
-                        <Route path="/inventory/add" element={<ItemAddPage />} />
-                        <Route path="*" element={<NotFoundPage />} />  */}
-                    </Routes>
-                    <Footer />
-                </BrowserRouter>
-            </>
-        );
-    }
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/inventory" element={<InventoryPage />} />
+                    <Route path="/warehouse" element={<WarehouseListPage />} />
+                    <Route path="/warehouse/add" element={<WarehouseAddPage />} />
+                    <Route path="/warehouses/:warehouseId/delete" element={<WarehouseDelete />} />
+                    {/* <Route path="/warehouse/:warehouseId" element={<WarehousePage />} /> */}
+                    <Route path="/warehouse/:warehouseId/edit" element={<WarehouseEditPage />} />
+                    {/* <Route path="/inventory/:itemId" element={<ItemPage />} />
+                    <Route path="/inventory/:itemId/edit" element={<ItemEditPage />} />
+                    <Route path="/inventory/add" element={<ItemAddPage />} />
+                    <Route path="*" element={<NotFoundPage />} /> */}
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </>
+    );
+}
 
 export default App;
