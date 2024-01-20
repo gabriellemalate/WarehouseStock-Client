fa1fe7d3ba5d63731414f7eebad709d371819659
Maria Rodriguez Hertz <marurh20@gmail.com>
2024-01-20 12:44:05 -0500

    style header for mobile

--- src/pages/WarehouseListPage/WarehouseListPage.js
+++ src/pages/WarehouseListPage/WarehouseListPage.js
@@ -33,60 +33,50 @@ function WarehouseListPage() {
     return (
         <>
             <main className="warehouses">
-                <div className="warehouses-eq">
-                    <section className="warehouses-top">
-                        <div className="warehouses-top-eq">
-                            <h1 className="warehouses-top__head">Warehouses</h1>
-
-                            <div className='warehouses-top__action'>
-
-                                <article className='warehouses-top__action-search'>
-                                    <form className="warehouses-top__action-search-eq">
-                                        <img className="warehouses-top__action-search-icon" src={MagnifyingGlass} alt="Search" />
-                                        <textarea className="warehouses-top__action-search-box" type="search" placeholder="Search..." />
-                                    </form>
-                                </article>
-                                <button className="warehouses-top__action-button" type='button' onClick={navigateToAddWarehouse}>
-                                    + Add New Warehouse
-                                </button>
+                <section className="warehouses-top">
+                    <h1 className="warehouses-top__head">Warehouses</h1>
+                    <form className="warehouses-top__action-search">
+                        <img className="warehouses-top__action-search-icon" src={MagnifyingGlass} alt="Search" />
+                        <input className="warehouses-top__action-search-box" type="search" placeholder="Search..." />
+                    </form>
+                    <button className="warehouses-top__action-button" type='button' onClick={navigateToAddWarehouse}>
+                        + Add New Warehouse
+                    </button>
+                </section>
+                <section className="warehouses-list">
+                    <div className="warehouses-list-eq">
+                        <article className="warehouses-list__tablet">
+                            <div className="warehouses-list__tablet-left">
+                                <div className="warehouses-list__tablet-label">
+                                    WAREHOUSE
+                                    <img className="warehouses-list__tablet-sort" alt="sort warehouses" src={Sort} />
+                                </div>
+                                <div className="warehouses-list__tablet-label">
+                                    ADDRESS
+                                    <img className="warehouses-list__tablet-sort" alt="sort addresses" src={Sort} />
+                                </div>
                             </div>
-                        </div>
-                    </section>
-                    <section className="warehouses-list">
-                        <div className="warehouses-list-eq">
-                            <article className="warehouses-list__tablet">
-                                <div className="warehouses-list__tablet-left">
-                                    <div className="warehouses-list__tablet-label">
-                                        WAREHOUSE
-                                        <img className="warehouses-list__tablet-sort" alt="sort warehouses" src={Sort} />
-                                    </div>
-                                    <div className="warehouses-list__tablet-label">
-                                        ADDRESS
-                                        <img className="warehouses-list__tablet-sort" alt="sort addresses" src={Sort} />
-                                    </div>
+                            <div className="warehouses-list__tablet-end">
+                            <div className="warehouses-list__tablet-right">
+                                <div className="warehouses-list__tablet-label">
+                                    CONTACT NAME
+                                    <img className="warehouses-list__tablet-sort" alt="sort contacts" src={Sort} />
                                 </div>
-                                <div className="warehouses-list__tablet-end">
-                                <div className="warehouses-list__tablet-right">
-                                    <div className="warehouses-list__tablet-label">
-                                        CONTACT NAME
-                                        <img className="warehouses-list__tablet-sort" alt="sort contacts" src={Sort} />
-                                    </div>
-                                    <div className="warehouses-list__tablet-label">
-                                        CONTACT INFORMATION
-                                        <img className="warehouses-list__tablet-sort" alt="sort contact information" src={Sort} />
-                                    </div>
+                                <div className="warehouses-list__tablet-label">
+                                    CONTACT INFORMATION
+                                    <img className="warehouses-list__tablet-sort" alt="sort contact information" src={Sort} />
                                 </div>
-                                <h3 className="warehouses-list__tablet-label warehouses-list__tablet-label-acts">
-                                    ACTIONS
-                                </h3>
-                                </div>
-                            </article>
-                            {warehouseList.map(
-                                (warehouse) => <WarehouseOption key={warehouse.id} warehouse={warehouse} />
-                            )}
-                        </div>
-                    </section>
-                </div>
+                            </div>
+                            <h3 className="warehouses-list__tablet-label warehouses-list__tablet-label-acts">
+                                ACTIONS
+                            </h3>
+                            </div>
+                        </article>
+                        {warehouseList.map(
+                            (warehouse) => <WarehouseOption key={warehouse.id} warehouse={warehouse} />
+                        )}
+                    </div>
+                </section>
             </main>
         </>
     );
