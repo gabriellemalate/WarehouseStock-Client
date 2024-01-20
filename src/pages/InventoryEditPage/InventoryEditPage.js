4dbc2345a7cf4b38e3309c8e7c8691e99f01e0f9
Soham Gaggenapally <55066921+sgaggen@users.noreply.github.com>
2024-01-20 13:02:22 -0500

    Set up files to work on inventory edit page

---
+++ src/pages/InventoryEditPage/InventoryEditPage.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { getInputError, formIsValid } from '../../utils/validationUtils';
+import errorIcon from '../../assets/icons/error-24px.svg'
+import backArrow from '../../assets/icons/arrow_back-24px.svg';
+import './InventoryEditPage.scss';
+
+function InventoryEditPage() {
+    return (
+        <main>
+            this is the main
+        </main>
+    )
+}
+
+
+
+export default InventoryEditPage;
