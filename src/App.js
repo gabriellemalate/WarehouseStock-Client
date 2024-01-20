40fd2dd974d0bf900eba298641b3f5e428e93130
Kyriakos Bakalis <greekgodsocrates.com>
2024-01-19 23:19:43 -0500

    app.js edit final

--- src/App.js
+++ src/App.js
@@ -8,26 +8,26 @@ import WarehouseAddPage from './pages/WarehouseAddPage/WarehouseAddPage';
 import Footer from "./components/Footer/Footer";
 import './App.scss';
 
-function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path="/inventory" element={<InventoryPage />} />
-                    <Route path="/warehouse" element={<WarehouseListPage />} />
-                    <Route path="/warehouse/add" element={<WarehouseAddPage />} />
-                    {/* <Route path="/warehouse/:warehouseId" element={<WarehouseDetailsPage />} /> */}
-                    <Route path="/warehouse/:warehouseId/edit" element={<WarehouseEditPage />} />
-                    {/* <Route path="/inventory/:itemId" element={<ItemPage />} />
-                        <Route path="/inventory/:itemId/edit" element={<ItemEditPage />} />
-                        <Route path="/inventory/add" element={<ItemAddPage />} />
-                        <Route path="*" element={<NotFoundPage />} /> */}
-                </Routes>
-                <Footer />
-            </BrowserRouter>
-        </>
-    );
-}
+    function App() {
+        return (
+            <>
+                <BrowserRouter>
+                    <Header />
+                    <Routes>
+                        <Route path="/inventory" element={<InventoryPage />} />
+                        <Route path="/warehouse" element={<WarehouseListPage />} />
+                        <Route path="/warehouse/add" element={<WarehouseAddPage />} />
+                        {/* <Route path="/warehouse/:warehouseId" element={<WarehouseDetailsPage />} /> */}
+                        <Route path="/warehouse/:warehouseId/edit" element={<WarehouseEditPage />} />
+                        {/* <Route path="/inventory/:itemId" element={<ItemPage />} />
+                            <Route path="/inventory/:itemId/edit" element={<ItemEditPage />} />
+                            <Route path="/inventory/add" element={<ItemAddPage />} />
+                            <Route path="*" element={<NotFoundPage />} /> */}
+                    </Routes>
+                    <Footer />
+                </BrowserRouter>
+            </>
+        );
+    }
 
 export default App;
