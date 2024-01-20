23b19fd2901e57f400cca00ffc784d98427b2365
Kyriakos Bakalis <greekgodsocrates.com>
2024-01-19 23:04:01 -0500

    formatting code update

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import WarehouseEditPage from './pages/WarehouseEditPage/WarehouseEditPage';
 import WarehouseDetailsPage from './pages/WarehouseDetailsPage/WarehouseDetailsPage';
 import WarehouseAddPage from './pages/WarehouseAddPage/WarehouseAddPage';
 import Footer from "./components/Footer/Footer";
-import './App.scss';	
+import './App.scss';
 
 function App() {
   return (
     <>
-       <BrowserRouter>
-                    <Header />
-                    <Routes>
-                        <Route path="/inventory" element={<InventoryPage />} />
-                        <Route path="/warehouse" element={<WarehouseListPage />} />
-                        <Route path="/warehouse/add" element={<WarehouseAddPage />} />
-                        {/* <Route path="/warehouse/:warehouseId" element={<WarehouseDetailsPage />} /> */}
-                        <Route path="/warehouse/:warehouseId/edit" element={<WarehouseEditPage />} />
-                        {/* <Route path="/inventory/:itemId" element={<ItemPage />} />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/inventory" element={<InventoryPage />} />
+          <Route path="/warehouse" element={<WarehouseListPage />} />
+          <Route path="/warehouse/add" element={<WarehouseAddPage />} />
+          {/* <Route path="/warehouse/:warehouseId" element={<WarehouseDetailsPage />} /> */}
+          <Route path="/warehouse/:warehouseId/edit" element={<WarehouseEditPage />} />
+          {/* <Route path="/inventory/:itemId" element={<ItemPage />} />
                         <Route path="/inventory/:itemId/edit" element={<ItemEditPage />} />
                         <Route path="/inventory/add" element={<ItemAddPage />} />
                         <Route path="*" element={<NotFoundPage />} /> */}
