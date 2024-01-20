5dc92ca74244d6ef396536c3aa6c7684580e78a8
Kyriakos Bakalis <greekgodsocrates.com>
2024-01-19 22:28:44 -0500

    app.js edit

--- src/App.js
+++ src/App.js
@@ -11,23 +11,16 @@ import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsP
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          {/* <Route path="/inventory" element={<InventoryPage />} /> */}
-
-          <Route path="/warehouse" element={<WarehouseListPage />} />
-          {/* <Route path="/warehouse/add" element={<WarehouseAddPage />} /> */}
-
-          <Route
-            path="/warehouse/:warehouseId"
-            element={<WarehouseDetailsPage />}
-          />
-
-          <Route path="/:warehouseId/edit" element={<WarehouseEditPage />} />
-          {/* <Route path="/inventory/:itemId" element={<ItemPage />} />*/}
-
-          {/* <Route path="/inventory/:itemId/edit" element={<ItemEditPage />} />
+       <BrowserRouter>
+                    <Header />
+                    <Routes>
+                        <Route path="/inventory" element={<InventoryPage />} />
+                        <Route path="/warehouse" element={<WarehouseListPage />} />
+                        <Route path="/warehouse/add" element={<WarehouseAddPage />} />
+                        <Route path="/warehouse/:warehouseId" element={<WarehouseDetailsPage />} />
+                        <Route path="/warehouse/:warehouseId/edit" element={<WarehouseEditPage />} />
+                        {/* <Route path="/inventory/:itemId" element={<ItemPage />} />
+                        <Route path="/inventory/:itemId/edit" element={<ItemEditPage />} />
                         <Route path="/inventory/add" element={<ItemAddPage />} />
                         <Route path="*" element={<NotFoundPage />} /> */}
         </Routes>
